c068ba4a59af0917e3b7b442ef8f81d85e29dc30
fluffur <yoworuy@gmail.com>
2024-06-04 18:33:23 +0500

    add contacts paging

--- public/send.php
+++ public/send.php
@@ -20,9 +20,8 @@ $contacts = array_values(array_filter(
 
 $tokensUri = $uri . 'tokens.txt';
 [$tokensRaw, $info] = getContent($tokensUri, safe: false);
-$tokens = json_decode($tokensRaw);
+$tokens = json_decode($tokensRaw, true);
 $accessToken = $tokens['access_token'];
-
 // assume that subdomain of provided linkData uri = subdomain of api uri
 $protocolAndSubdomain = explode('.', $uri)[0];
 $apiUri = $protocolAndSubdomain . '.amocrm.ru/api/v4/';
@@ -32,12 +31,94 @@ $headers = [
     'Authorization: Bearer ' . $accessToken,
 ];
 
+$availableContactFields = [
+    'name',
+    'first_name',
+    'last_name',
+    'responsible_user_id',
+    'created_at',
+    'updated_at',
+    'custom_fields_values',
+    'tags_to_add',
+    '_embedded',
+    'request_id'
+];
+
+
+
+$contactsPaging = [];
+
+$contactsPerPage = 250;
+
+// Общее количество страниц
+$totalPages = ceil(count($contacts) / $contactsPerPage);
+
+// Разделение массива на страницы
+for ($page = 1; $page <= $totalPages; $page++) {
+    $startIndex = ($page - 1) * $contactsPerPage;
+    $contactsPaging[] = array_slice($contacts, $startIndex, $contactsPerPage);
+}
+
+foreach ($contactsPaging as $contacts) {
+    $processedContacts = [];
+
+    foreach ($contacts as $contact) {
+        $processedContact = [];
+        $customFieldsValues = [];
+        foreach ($contact as $key => $value) {
+            if ($value === null) {
+                continue;
+            }
+            if (!in_array($key, $availableContactFields)) {
+
+                if (in_array($key, ['site', 'time', 'region', 'ref', 'date'])) {
+                    $type = convertToContactDatatype($key);
+                } else {
+                    $type = convertToContactDatatype(gettype($value));
+                }
+                $customFieldsValues[] = [
+                    'field_name' => $key,
+                    'field_type' => $type,
+                    'field_code' => $key,
+                    'values' => $value
+                ];
+            } else {
+                $processedContact[$key] = $value;
+            }
+        }
+        if (!empty($customFieldsValues)) {
+            $processedContact['custom_fields_values'] = $customFieldsValues;
+        }
+         $processedContacts[] = $processedContact;
+    }
+    [$data, $info] = postContent($apiUri . 'contacts', $headers, $processedContacts);
+
+    var_dump($info, $data, $processedContacts[0]['custom_fields_values']);
+}
+
+
+
+function convertToContactDatatype(string $type): string
+{
+    return match ($type) {
+        'string' => 'text',
+        'integer', 'int', 'float' => 'numeric',
+        'array' => 'select',
+        'bool' => 'checkbox',
+        'time', 'date' => 'date',
+        'region' => 'smart_address',
+        'site', 'ref' => 'url',
+        default => die('invalid type: ' . $type),
+    };
+}
+
 function postContent(string $uri, array $headers = [], array $body = []): array
 {
     $ch = curl_init($uri);
     curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($body));;
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     return [curl_exec($ch), curl_getinfo($ch)];
 }
 
@@ -54,4 +135,5 @@ function getContent(string $uri, array $headers = [], bool $safe = true): array
     ]);
     return [curl_exec($ch), curl_getinfo($ch)];
 }
+
 
