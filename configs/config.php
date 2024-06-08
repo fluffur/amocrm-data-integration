51e12e3cc60ccd045900037ae387e45bda310bba
fluffur <yoworuy@gmail.com>
2024-06-08 18:42:12 +0500

    change naming

--- configs/config.php
+++ configs/config.php
@@ -13,10 +13,9 @@ function getEnvironmentValue($key, $default = null)
 
 define("ACCESS_TOKEN", getEnvironmentValue('ACCESS_TOKEN'));
 define("SUBDOMAIN", getEnvironmentValue('AMOCRM_SUBDOMAIN'));
-
 const AMOCRM_HEADERS = [
     'Content-Type: application/json',
     'Authorization: Bearer ' . ACCESS_TOKEN
 ];
-const BASE_URI = 'https://' . SUBDOMAIN . '.amocrm.ru/';
-const AMOCRM_API_URI = BASE_URI . '/api/v4';
+const AMOCRM_BASE_URI = 'https://' . SUBDOMAIN . '.amocrm.ru/';
+const AMOCRM_API_URI = AMOCRM_BASE_URI . '/api/v4';
